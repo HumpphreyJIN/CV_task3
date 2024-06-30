1a032c87931c13cf9a78a2e4cdc0e456d47847a9
HumpphreyJIN <151526982+HumpphreyJIN@users.noreply.github.com>
2024-06-30 13:32:15 +0800

    Update u_train.py

--- task1/u_train.py
+++ task1/u_train.py
@@ -3,8 +3,8 @@ import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
-from task1_model import SimCLRStage1, SimCLRStage2
-from task1_pre import PreDataset_CIFAR100, train_transform, test_transform
+from model import SimCLRStage1, SimCLRStage2
+from pre import PreDataset_CIFAR100, train_transform, test_transform
 
 pth_epoch = 300 #加载训练了多少轮的权重
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
